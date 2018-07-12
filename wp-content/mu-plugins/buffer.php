7f78284824480d93d75f1d26b5c80f8213c1c2d3
idevol <saul.diaz.v@gmail.com>
2018-07-12 11:56:17 -0500

    Soporte HTTPS y optimizacion de cache
    Elimina query vars version y ver del dominio del sitio

--- wp-content/mu-plugins/buffer.php
+++ wp-content/mu-plugins/buffer.php
@@ -27,3 +27,33 @@ add_action(
 	}, 
 	0
 );
+
+add_filter('final_output', function($output) {
+	// Soporte HTTPS
+	$output = str_replace('http:', 'https:', $output);
+
+	// Corrige errores con esquemas establecidos en http y no https
+	$output = str_replace('https://schemas.xmlsoap.org', 'http://schemas.xmlsoap.org', $output);
+	$output = str_replace('https://docs.oasisopen.org', 'http://docs.oasisopen.org', $output);
+	$output = str_replace('https://www.sitemaps.org', 'http://www.sitemaps.org', $output);
+	
+	// Optimizacion de cache, elimina query vars de version y ver
+	$patt = array(
+	'/(("|\')https?:\/\/((emfoco\.idevol\.net)|(emfocoydesarrollo\.com))\/[a-zA-Z0-9\._\/-]*\.(css|js))(\?(ver|version)=[a-zA-Z0-9%_.-]{1,8})("|\')/'
+	,'/(("|\')https?:\\\\\/\\\\\/((emfoco\.idevol\.net)|(emfocoydesarrollo\.com))\\\\\/[a-zA-Z0-9\._\\\\\/-]*\.(css|js))(\?(ver|version)=[a-zA-Z0-9%_.-]{1,8})("|\')/'
+	);
+	$repl = array(
+	'${1}${9}'
+	,'${1}${9}'
+	);
+	$output = preg_replace($patt, $repl, $output);    
+	
+	return $output;
+});
+
+add_filter('wp_redirect', function($output) {
+	// Redireccionamientos 301 en https
+	$output = str_replace('http://emfoco.idevol.net', 'https://emfoco.idevol.net', $output);
+	$output = str_replace('http://emfocoydesarrollo.com', 'https://emfocoydesarrollo.com', $output);
+	return $output;
+});
