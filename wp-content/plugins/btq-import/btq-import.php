63f13e3887db636b128deb797b63c7638c0ba604
Saul <saul.diaz.v@gmail.com>
2018-07-12 14:02:36 -0500

    sanitize_text_field

--- wp-content/plugins/btq-import/btq-import.php
+++ wp-content/plugins/btq-import/btq-import.php
@@ -100,8 +100,8 @@ function btq_import_admin_page(){
 	
 	foreach ( $results as $row ) 
 	{
-		echo $row->titulo . '<br><br>';
-		echo $row->nota_completa . '<br><br><br><br>';
+		echo sanitize_text_field($row->titulo) . '<br>';
+		echo sanitize_text_field($row->nota_completa) . '<br><br><br><br>';
 	}
 	
 	$dbcn->print_error();
