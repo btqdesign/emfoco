f77a63d06b1ae31698488849493329e7e3cac179
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-18 12:16:13 -0500

    taxonomia

--- wp-content/plugins/plugin emfoco/emfoco.php
+++ wp-content/plugins/plugin emfoco/emfoco.php
@@ -37,30 +37,50 @@ function btq_emfoco_VC() {
 	));
 }
 add_action( 'vc_before_init', 'btq_emfoco_VC' );
+
+
+
+
+    //hook into the init action and call create_book_taxonomies when it fires
+    add_action( 'init', 'create_topics_hierarchical_taxonomy', 0 );
  
 //create a custom taxonomy name it topics for your posts
  
 function create_topics_hierarchical_taxonomy() {
-    // Hooking up our function to theme setup
-    add_action( 'init', 'create_posttype' );
-     
+ 
+// Add new taxonomy, make it hierarchical like categories
+//first do the translations part for GUI
+ 
+  $labels = array(
+    'name' => _x( 'Topics', 'taxonomy general name' ),
+    'singular_name' => _x( 'Topic', 'taxonomy singular name' ),
+    'search_items' =>  __( 'Search Topics' ),
+    'all_items' => __( 'All Topics' ),
+    'parent_item' => __( 'Parent Topic' ),
+    'parent_item_colon' => __( 'Parent Topic:' ),
+    'edit_item' => __( 'Edit Topic' ), 
+    'update_item' => __( 'Update Topic' ),
+    'add_new_item' => __( 'Add New Topic' ),
+    'new_item_name' => __( 'New Topic Name' ),
+    'menu_name' => __( 'Topics' ),
+  );    
+ 
 
-    function sectores_init() {
-        // create a new taxonomy
-        register_taxonomy(
-            'Sectores',
-            'post',
-            array(
-                'label' => __( 'Sectores' ),
-                'rewrite' => array( 'slug' => 'Sector' ),
-                'capabilities' => array(
-                    'assign_terms' => 'edit_guides',
-                    'edit_terms' => 'publish_guides'
-                )
+  function sectores_init() {
+    // create a new taxonomy
+    register_taxonomy(
+        'Sectores',
+        'post',
+        array(
+            'label' => __( 'Sectores' ),
+            'rewrite' => array( 'slug' => 'Sector' ),
+            'capabilities' => array(
+                'assign_terms' => 'edit_guides',
+                'edit_terms' => 'publish_guides'
             )
-        );
-    }
+        )
+    );
 }
 add_action( 'init', 'sectores_init' );
-
-
+ 
+}
