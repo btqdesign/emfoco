7a4236538457d1309b8a998c79af4eacb37b80e0
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-18 13:00:46 -0500

    posttype

--- wp-content/plugins/plugin emfoco/emfoco.php
+++ wp-content/plugins/plugin emfoco/emfoco.php
@@ -38,6 +38,53 @@ function btq_emfoco_VC() {
 }
 add_action( 'vc_before_init', 'btq_emfoco_VC' );
 
+
+
+function create_posttype() {
+
+    $labels = array(
+        'name'                => _x( 'Productores', 'Post Type General Name', 'twentythirteen' ),
+        'singular_name'       => _x( 'Productor', 'Post Type Singular Name', 'twentythirteen' ),
+        'menu_name'           => __( 'Productores', 'twentythirteen' ),
+        'parent_item_colon'   => __( 'Prodcutor padre', 'twentythirteen' ),
+        'all_items'           => __( 'Todos los productores', 'twentythirteen' ),
+        'view_item'           => __( 'Ver productor', 'twentythirteen' ),
+        'add_new_item'        => __( 'Añadir nuevo productor', 'twentythirteen' ),
+        'add_new'             => __( 'Añadir nuevo', 'twentythirteen' ),
+        'edit_item'           => __( 'Editar productor', 'twentythirteen' ),
+        'update_item'         => __( 'Actualizar productor', 'twentythirteen' ),
+        'search_items'        => __( 'Buscar productor', 'twentythirteen' ),
+        'not_found'           => __( 'No se encontro', 'twentythirteen' ),
+        'not_found_in_trash'  => __( 'No se encontro en la', 'twentythirteen' ),
+    );
+
+    register_post_type( 'Productores',
+            // CPT Options
+                array(
+                    'labels' => array(
+                        'name' => __( 'Productores' ),
+                        'singular_name' => __( 'Productor' )
+                    ),
+                    'public'              => true,
+                    'has_archive'         => true,
+                    'show_ui'             => true,
+                    'show_in_menu'        => true,
+                    'show_in_nav_menus'   => true,
+                    'show_in_admin_bar'   => true,
+                    'menu_position'       => 5,
+                    'can_export'          => true,
+                    'has_archive'         => true,
+                    'exclude_from_search' => false,
+                    'publicly_queryable'  => true,
+                    'rewrite' => array('slug' => 'Productor'),
+                )
+            );
+        }
+    add_action( 'init', 'create_posttype' );
+
+
+
+
     //hook into the init action and call create_book_taxonomies when it fires
 add_action( 'init', 'create_topics_hierarchical_taxonomy', 0 );
  
@@ -74,68 +121,3 @@ function create_topics_hierarchical_taxonomy() {
   ));
  
 }
-
-
-
-/*
-* Creating a function to create our CPT
-*/
- 
-function custom_post_type() {
- 
-    // Set UI labels for Custom Post Type
-        $labels = array(
-            'name'                => _x( 'Movies', 'Post Type General Name', 'twentythirteen' ),
-            'singular_name'       => _x( 'Movie', 'Post Type Singular Name', 'twentythirteen' ),
-            'menu_name'           => __( 'Movies', 'twentythirteen' ),
-            'parent_item_colon'   => __( 'Parent Movie', 'twentythirteen' ),
-            'all_items'           => __( 'All Movies', 'twentythirteen' ),
-            'view_item'           => __( 'View Movie', 'twentythirteen' ),
-            'add_new_item'        => __( 'Add New Movie', 'twentythirteen' ),
-            'add_new'             => __( 'Add New', 'twentythirteen' ),
-            'edit_item'           => __( 'Edit Movie', 'twentythirteen' ),
-            'update_item'         => __( 'Update Movie', 'twentythirteen' ),
-            'search_items'        => __( 'Search Movie', 'twentythirteen' ),
-            'not_found'           => __( 'Not Found', 'twentythirteen' ),
-            'not_found_in_trash'  => __( 'Not found in Trash', 'twentythirteen' ),
-        );
-         
-    // Set other options for Custom Post Type
-         
-        $args = array(
-            'label'               => __( 'movies', 'twentythirteen' ),
-            'description'         => __( 'Movie news and reviews', 'twentythirteen' ),
-            'labels'              => $labels,
-            // Features this CPT supports in Post Editor
-            'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),
-            // You can associate this CPT with a taxonomy or custom taxonomy. 
-            'taxonomies'          => array( 'genres' ),
-            /* A hierarchical CPT is like Pages and can have
-            * Parent and child items. A non-hierarchical CPT
-            * is like Posts.
-            */ 
-            'hierarchical'        => false,
-            'public'              => true,
-            'show_ui'             => true,
-            'show_in_menu'        => true,
-            'show_in_nav_menus'   => true,
-            'show_in_admin_bar'   => true,
-            'menu_position'       => 5,
-            'can_export'          => true,
-            'has_archive'         => true,
-            'exclude_from_search' => false,
-            'publicly_queryable'  => true,
-            'capability_type'     => 'page',
-        );
-         
-        // Registering your Custom Post Type
-        register_post_type( 'movies', $args );
-     
-    }
-     
-    /* Hook into the 'init' action so that the function
-    * Containing our post type registration is not 
-    * unnecessarily executed. 
-    */
-     
-    add_action( 'init', 'custom_post_type', 0 );
