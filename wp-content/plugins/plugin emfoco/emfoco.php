cb10d13d4fc64448c16e0e4fcbb3ec4c41946ad8
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-19 13:23:54 -0500

    prueba de la taxonomia

--- wp-content/plugins/plugin emfoco/emfoco.php
+++ wp-content/plugins/plugin emfoco/emfoco.php
@@ -40,6 +40,11 @@ add_action( 'vc_before_init', 'btq_emfoco_VC' );
 
 
 
+
+
+
+
+
 function create_posttype() {
 
     $labels = array(
@@ -79,51 +84,53 @@ function create_posttype() {
                     'rewrite' => array('slug' => 'Productor'),
                 )
             );
+
+
+                                //hook into the init action and call create_book_taxonomies when it fires
+                add_action( 'init', 'create_topics_hierarchical_taxonomy', 0 );
+                
+                //create a custom taxonomy name it topics for your posts
+                
+                function create_topics_hierarchical_taxonomy() {
+                
+                // Add new taxonomy, make it hierarchical like categories
+                //first do the translations part for GUI
+                
+                $labels = array(
+                    'name' => _x( 'Sectores', 'taxonomy general name' ),
+                    'singular_name' => _x( 'Sector', 'taxonomy singular name' ),
+                    'search_items' =>  __( 'Buscar en: sectores' ),
+                    'all_items' => __( 'Todos los sectores' ),
+                    'parent_item' => __( 'Sector padre' ),
+                    'parent_item_colon' => __( 'Sector padre:' ),
+                    'edit_item' => __( 'Editar Sector' ), 
+                    'update_item' => __( 'Actualizar Sector' ),
+                    'add_new_item' => __( 'Añadir Sector' ),
+                    'new_item_name' => __( 'Nuevo nombres del Sector' ),
+                    'menu_name' => __( 'Sectores' ),
+                );    
+                
+                // Now register the taxonomy
+                
+                register_taxonomy('Sectores',array('post'), array(
+                    'hierarchical' => true,
+                    'labels' => $labels,
+                    'show_ui' => true,
+                    'show_admin_column' => true,
+                    'query_var' => true,
+                    'rewrite' => array( 'slug' => 'Sector' ),
+                ));
+                
+                }
+
+                $args = array(
+                'public'   => true,
+                '_builtin' => false
+                
+                ); 
+
+
+
+
         }
     add_action( 'init', 'create_posttype' );
-
-
-
-
-    //hook into the init action and call create_book_taxonomies when it fires
-add_action( 'init', 'create_topics_hierarchical_taxonomy', 0 );
- 
-//create a custom taxonomy name it topics for your posts
- 
-function create_topics_hierarchical_taxonomy() {
- 
-// Add new taxonomy, make it hierarchical like categories
-//first do the translations part for GUI
- 
-  $labels = array(
-    'name' => _x( 'Sectores', 'taxonomy general name' ),
-    'singular_name' => _x( 'Sector', 'taxonomy singular name' ),
-    'search_items' =>  __( 'Buscar en: sectores' ),
-    'all_items' => __( 'Todos los sectores' ),
-    'parent_item' => __( 'Sector padre' ),
-    'parent_item_colon' => __( 'Sector padre:' ),
-    'edit_item' => __( 'Editar Sector' ), 
-    'update_item' => __( 'Actualizar Sector' ),
-    'add_new_item' => __( 'Añadir Sector' ),
-    'new_item_name' => __( 'Nuevo nombres del Sector' ),
-    'menu_name' => __( 'Sectores' ),
-  );    
- 
-// Now register the taxonomy
- 
-  register_taxonomy('Sectores',array('post'), array(
-    'hierarchical' => true,
-    'labels' => $labels,
-    'show_ui' => true,
-    'show_admin_column' => true,
-    'query_var' => true,
-    'rewrite' => array( 'slug' => 'Sector' ),
-  ));
- 
-}
-
-$args = array(
-  'public'   => true,
-  '_builtin' => false
-   
-); 
