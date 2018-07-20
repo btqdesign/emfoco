8dbd8e1458e2a871508e916636b756805f451500
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-20 13:16:33 -0500

    comment

--- wp-content/themes/fallsky-child/<?php /*  * Template Name: Featured Article  * Template Post Type: post, page, product  */wpb-single-post.php
+++ wp-content/themes/fallsky-child/<?php /*  * Template Name: Featured Article  * Template Post Type: post, page, product  */wpb-single-post.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Template Name: Full width para productores
- * Template Post Type: post, page, product
+ * Template Post Type: post, page, product, sectores, productores
  */
   
  get_header();  ?>
