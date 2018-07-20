3c0d1fe6b69507e66b2f75461f509ed508f28a36
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-20 14:14:31 -0500

    test

--- wp-content/themes/fallsky/index.php
+++ wp-content/themes/fallsky/index.php
@@ -8,7 +8,6 @@ get_header(); ?>
 	<div class="main">
 		<div class="container">
 			<div id="primary" class="content-area"><?php fallsky_main_content(); ?></div>
-			<?php get_sidebar(); ?>
 		</div> 
 	</div>
 
