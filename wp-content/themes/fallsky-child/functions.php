3fa97f6eba3ebf6e0b2ce2f500fb46b82e48bebb
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-20 13:01:07 -0500

    test

--- wp-content/themes/fallsky-child/functions.php
+++ wp-content/themes/fallsky-child/functions.php
@@ -1,13 +1,5 @@
 <?php
-	add_action('wp_enqueue_scripts', 'fallsky_child_enqueue_scripts');
-	function fallsky_child_enqueue_scripts(){
-		wp_enqueue_style('fallsky-child-theme-style', get_stylesheet_uri(), array('fallsky-theme-style'));
-	}
-
-	add_filter('fallsky_inline_style_handler', 'fallsky_child_inline_style_handler', 999);
-	function fallsky_child_inline_style_handler($handler){
-		return 'fallsky-child-theme-style';
-	}
+	
 
 
 
