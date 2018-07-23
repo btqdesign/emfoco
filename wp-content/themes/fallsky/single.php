dd92e00b997910d1eed32dffffb0926d15989d88
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-07-23 13:01:06 -0500

    Revert "sidebar"
    This reverts commit 9a6c6ff43b18cb569af2bffa5320e6e3d3d839fe.

--- wp-content/themes/fallsky/single.php
+++ wp-content/themes/fallsky/single.php
@@ -8,6 +8,7 @@ get_header(); ?>
 
 	<div class="main">
 		<?php get_template_part( 'template-parts/single', get_post_type() ); ?>
-		<?php get_sidebar(); ?>
 	</div>
+	<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
