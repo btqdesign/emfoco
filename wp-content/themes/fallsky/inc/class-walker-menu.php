a67eefa25c8fc8f5b813d10237161b852d812c61
JoseGarciaDelCarmen <39064016+JoseGarciaDelCarmen@users.noreply.github.com>
2018-08-09 12:38:11 -0500

    error_log("objeto:" .$item->object);

--- wp-content/themes/fallsky/inc/class-walker-menu.php
+++ wp-content/themes/fallsky/inc/class-walker-menu.php
@@ -120,6 +120,7 @@ if(class_exists('Walker_Nav_Menu')){
 			$output .= "{$n}{$indent}<ul class=\"sub-menu\" style=\"display: none;\">{$n}";
 		}
 		private function is_mega_category($item, $depth){
+			error_log("objeto:" .$item->object);
 			return in_array('mega-menu', (array)$item->classes) && ($depth == 0) && ($item->object == 'category' || $item->object == 'sector');
 		}
 		private function post_list($query, $before = '', $after = ''){
